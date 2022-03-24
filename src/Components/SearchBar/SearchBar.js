26032af508b99256e4e6bf291c4aece9d5705c07
james4-daf <d.james@elsevier.com>
2022-03-24 11:53:48 +0000

    complete without deploy

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -5,18 +5,18 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTerm: "",
+      term: "",
     };
     this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
   }
-  search() {
-    this.props.onSearch(this.props.searchTerm);
-  }
   handleTermChange(event) {
     this.setState({
-      searchTerm: event.target.value,
+      term: event.target.value,
     });
+  }
+  search() {
+    this.props.onSearch(this.state.term);
   }
   render() {
     return (
@@ -25,7 +25,9 @@ class SearchBar extends React.Component {
           placeholder="Enter A Song, Album, or Artist"
           onChange={this.handleTermChange}
         />
-        <button className="SearchButton">SEARCH</button>
+        <button onClick={this.search} className="SearchButton">
+          SEARCH
+        </button>
       </div>
     );
   }
